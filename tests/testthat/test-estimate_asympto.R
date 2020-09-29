a2a4c6d87371fd3dc1f5368933821d3c30ce2ec9
Hugo Gruson <hugo.gruson@protonmail.com>
2020-09-29 14:41:01 +0200

    Add tests

---
+++ tests/testthat/test-estimate_asympto.R
@@ -0,0 +1,28 @@
+test_that("estimate_asympto", {
+
+  d <- readRDS(system.file("extdata", "covid19_ny.rds", package = "asymptor"))
+
+  a_up <- estimate_asympto(d, bounds = "upper")
+  a_lw <- estimate_asympto(d, bounds = "lower")
+  a_both <- estimate_asympto(d)
+
+  expect_identical(dim(a_up), c(nrow(d), 2L))
+  expect_identical(dim(a_lw), c(nrow(d), 2L))
+  expect_identical(dim(a_both), c(nrow(d), 3L))
+
+  expect_identical(merge(a_lw, a_up), a_both)
+
+  expect_false(any(a_both[, c(2,3)]<0, na.rm = TRUE))
+
+  d_nc <- d[, c("date", "cases", "deaths")]
+
+  expect_error(
+    estimate_asympto(d_nc),
+    "recoveries"
+  )
+
+  expect_identical(
+    estimate_asympto(d_nc, bounds = "lower"),
+    a_lw
+  )
+})
