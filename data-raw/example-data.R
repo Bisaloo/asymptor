89802b52d2e1630540cbd64c12f5a3fbc761cc57
Hugo Gruson <hugo.gruson@protonmail.com>
2020-09-30 14:18:04 +0200

    Add germany test data

--- data-raw/example-data.R
+++ data-raw/example-data.R
@@ -5,10 +5,27 @@ df_all <- read.csv("https://storage.googleapis.com/covid19-open-data/v2/epidemio
 library(dplyr)
 
 df_it <- df_all %>%
-  na.omit() %>%
   filter(key == "IT") %>%
   transmute(date = as.Date(date),
             new_cases = new_confirmed,
             new_deaths = new_deceased)
 
 saveRDS(df_it, "inst/extdata/covid19_italy.rds")
+
+df_de <- df_all %>%
+  filter(key == "DE") %>%
+  transmute(date = as.Date(date),
+            new_cases = new_confirmed,
+            new_deaths = new_deceased)
+
+saveRDS(df_de, "inst/extdata/covid19_germany.rds")
+
+
+df_de %>%
+  estimate_asympto() %>%
+  merge(df_de) %>%
+  filter(date <= "2020-04-17") %>%
+  select(-date) %>%
+  colSums(na.rm = TRUE)
+
+
