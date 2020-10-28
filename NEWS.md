35466681cf7e925fc87ba25b585892b5d4146ef9
Hugo Gruson <hugo.gruson@protonmail.com>
2020-10-28 09:19:04 +0100

    Use noRd on internal function

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # asymptor 0.3
 
+* Unexported functions now use the `@noRd` roxygen tag, which mean they will
+not appear in the manual
 * Fixed citation
 
 # asymptor 0.2
--- R/custom_lag.R
+++ R/custom_lag.R
@@ -6,11 +6,13 @@
 #' @return A lagged vector (not a time series as opposed to [stats::lag()]).
 #' The behaviour of this `lag()` function is closer to that of `dplyr::lag()`.
 #'
-# @examples
-# c(custom_lag(c(1,3,5), 1))
-#
-# c(custom_lag(LETTERS, 5))
-#
+#' @examples
+#' c(custom_lag(c(1,3,5), 1))
+#'
+#' c(custom_lag(LETTERS, 5))
+#'
+#' @noRd
+#'
 custom_lag <- function(x, k = 1) {
 
   c(rep_len(NA, k), x[seq_len(length(x) - k)])
